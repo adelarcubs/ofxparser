bfa16e116891123ec0547391def74fea483c941b
Adelar Tiemann Junior <adelar@adelarcubs.com>
2016-09-26 20:37:37 -0300

    Colocando mais dados do arquivo OFX

--- src/OfxMovement.php
+++ src/OfxMovement.php
@@ -2,6 +2,7 @@
 namespace Adelarcubs\OFXParser;
 
 use SimpleXMLElement;
+use Datatime;
 
 /**
  *
@@ -10,15 +11,39 @@ use SimpleXMLElement;
 class OfxMovement
 {
 
+    private $document;
+
     private $description;
+
+    private $amount;
+
+    private $dueDate;
 
     public function __construct(SimpleXMLElement $xml)
     {
+        $this->document = $xml->FITID;
         $this->description = $xml->MEMO;
+        $this->amount = (float) str_replace(',', '.', $xml->TRNAMT);
+        $this->dueDate = new DateTime(substr($this->DTPOSTED, 0, 8));
+    }
+
+    public function getDocument()
+    {
+        return $this->document;
     }
 
     public function getDescription()
     {
         return $this->description;
+    }
+
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    public function getDueDate()
+    {
+        return $this->dueDate;
     }
 }
